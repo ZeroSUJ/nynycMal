43c00938eca57661980675ba7100f180372731b5
coin5555 <andersn0098@gmail.com>
2024-03-09 00:11:51 +1000

    none image

--- frontend/components/miner.tsx
+++ frontend/components/miner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Miners(props: any) {
     return (
         <div className="flex items-center Card rounded-2xl bg-[#27272a] py-2 px-4">
-            <img
+            {/* <img
                 alt="mining"
                 loading="lazy"
                 width="45"
@@ -12,7 +12,7 @@ export default function Miners(props: any) {
                 data-nimg="1"
                 className="mr-1 h-10 w-[50px] lg:mr-4"
                 src={props.url}
-            />
+            /> */}
             <h3 className="flex-grow text-xl">{props.name}</h3>
             <span className="ml-2 text-xl font-bold text-blue-500">{props.number}</span>
             <span className="ml-2 text-xl font-bold">{props.unit}</span>
